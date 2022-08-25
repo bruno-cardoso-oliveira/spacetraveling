0d62353af5a28e05764a527e7b8355996d1319c5
Bruno Cardoso <buninho02@hotmail.com>
2022-08-25 19:30:28 -0300

    update

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,10 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { RichText } from 'prismic-dom';
+
 
 interface Post {
   first_publication_date: string | null;
@@ -27,10 +33,69 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post() {
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
+
+  if(router.isFallback) { // se o post ainda não foi carregado, vai exibir um carregando em tela...
+    return <h1>Carregando...</h1>
+  }
+
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    const headingTime = contentItem.heading.split(/\s+/).length;
+    const wordsTime = RichText.asText(contentItem.body).split(/\s+/).length;
+
+    return total + headingTime + wordsTime;
+  }, 0);
+
+  const readTime = Math.ceil(totalWords / 200);
+
+  const formattedDate = format(
+    new Date(post.first_publication_date), 
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  )
+  
   return (
     <>
       <Header />
+      <img src={post.data.banner.url} alt="Banner" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>{post.data.title}</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                {formattedDate}
+              </li>
+              <li>
+                <FiUser />
+                {post.data.author}
+              </li>
+              <li>
+                <FiClock />
+                {`${readTime} min`}
+              </li>
+            </ul>
+          </div>
+
+          {post.data.content.map(content => {
+            return (
+              <article key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div 
+                  className={styles.postContent}
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body)
+                  }}    
+                />
+              </article>
+            )
+          })}
+        </div>
+      </main>
     </>
   )
 }
